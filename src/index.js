29fdd033ce96bca1b75e7cb3171f5a5ba0c8ab2b
Arpit1815Coder <arpityadav1518@gmail.com>
2024-06-09 12:53:20 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import App from './App.js';
+import App from './App.jsx';
 import Form from './Form.js';
 import Success from './Success.js';
 import './index.css';
